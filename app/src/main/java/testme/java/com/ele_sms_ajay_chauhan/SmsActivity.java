8c90cd37dc6f76ce571588326bd0051bb302edcf
Ajay Chauhan <achauhan142@gmail.com>
2018-03-01 22:57:45 +0530

    Read Inbox Message Permission'

--- app/src/main/java/testme/java/com/ele_sms_ajay_chauhan/SmsActivity.java
+++ app/src/main/java/testme/java/com/ele_sms_ajay_chauhan/SmsActivity.java
@@ -1,13 +1,99 @@
 package testme.java.com.ele_sms_ajay_chauhan;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
+import android.support.constraint.ConstraintLayout;
+
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class SmsActivity extends AppCompatActivity {
+
+    @BindView(R.id.text)
+    TextView textView;
+
+    Cursor cursor;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sms);
+        ButterKnife.bind(this);
+
+        requestPermissions();
+
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        if (cursor != null)
+            cursor.close();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
+
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    readMessages();
+
+                } else {
+                    Toast.makeText(this, "Permission denied to read your Messages", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+        }
+    }
+
+    private void readMessages() {
+
+        cursor = getContentResolver().query(Uri.parse("content://sms/inbox"), null, null, null, null);
+
+        if (cursor != null && cursor.moveToFirst()) {
+            cursor.getColumnCount();
+            do {
+                String msgData = "";
+                for (int idx = 0; idx < cursor.getColumnCount(); idx++) {
+                    msgData += " " + cursor.getColumnName(idx) + ":" + cursor.getString(idx);
+                    textView.setText(msgData);
+                }
+            } while (cursor.moveToNext());
+        } else {
+
+        }
+    }
+
+    private void requestPermissions() {
+        ActivityCompat.requestPermissions(SmsActivity.this,
+                new String[]{Manifest.permission.READ_SMS},
+                1);
+    }
+
+
 }
---
+++ app/src/main/java/testme/java/com/ele_sms_ajay_chauhan/model/SmsModel.java
@@ -0,0 +1,8 @@
+package testme.java.com.ele_sms_ajay_chauhan.model;
+
+/**
+ * Created by achau on 01-03-2018.
+ */
+
+public class SmsModel {
+}
